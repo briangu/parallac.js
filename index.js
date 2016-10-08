27caefab584a677a67b8640410e548915ec51c0c
Brian Guarraci <brian@crickethealth.com>
2016-10-07 18:59:23 -0700

    add basic locale

--- index.js
+++ index.js
@@ -30,49 +30,27 @@ const baseSandbox = {
   console: console
 }
 
-// const sandbox1 = createChildContext(baseSandbox)
-// sandbox1.a = 1
-
-// const sandbox2 = createChildContext(sandbox1)
-
 var vm = require('vm');
-// var context1 = vm.createContext(sandbox1);
-// var context2 = vm.createContext(sandbox2);
-// console.log("context1", context1.a)
-// console.log("context2", context2.a)
-// console.log("sandbox1", sandbox1.a)
-// console.log("sandbox2", sandbox2.a)
-//
-// // var script = new vm.Script("var fn = " + code + ";fn();");
-// var script = new vm.Script(code + ";fn();");
-//
-// console.time('vm');
-// script.runInContext(context1);
-// console.timeEnd('vm');
-//
-// console.log("context1", context1.a)
-// console.log("context2", context2.a)
-// console.log("sandbox1", sandbox1.a)
-// console.log("sandbox2", sandbox2.a)
 
 function on(locale) {
-  var sandbox = createChildContext(baseSandbox)
   return {
-    use: function(obj) {
-      sandbox = createChildContext(sandbox, obj)
+    with: function (obj) {
+      locale.pushContext(obj)
       return this;
     },
-    then: function(fn) {
-      return new Promise(function(resolve, reject) {
+    run: function (fn) {
+      return new Promise(function (resolve, reject) {
         try {
           const code = "result = (" + fn.toString() + ")();"
           console.log(code);
           var script = new vm.Script(code);
           console.time('vm');
-          const resultSandbox = createChildContext(sandbox, { result: undefined })
-          script.runInNewContext(resultSandbox);
+          locale.pushContext({
+            result: undefined
+          })
+          script.runInNewContext(locale.currentContext());
           console.timeEnd('vm');
-          resolve(resultSandbox.result)
+          resolve(locale.currentContext().result)
         } catch (err) {
           reject(err);
         }
@@ -81,11 +59,41 @@ function on(locale) {
   }
 }
 
-const Locales = [1]
+var Locale = function(id) {
+  this.id = id
+  this.baseContext = createChildContext(baseSandbox)
+
+  // TODO: scope by session
+  this.contexts = [this.baseContext];
+
+  this.pushContext = function(context) {
+    const parentContext = this.currentContext()
+    const childContext = createChildContext(parentContext, context)
+    this.contexts.push(childContext)
+    return childContext
+  }
+
+  this.popContext = function() {
+    return this.contexts.pop()
+  }
+
+  this.clearContext = function() {
+    this.contexts = [this.baseContext]
+  }
+
+  this.currentContext = function() {
+    return this.contexts[this.contexts.length - 1]
+  }
+}
+
+const Locales = []
+Locales.push(new Locale(1))
 
 on(Locales[0])
-  .use({a: 1})
-  .then(() => {
+  .with({
+    a: 1
+  })
+  .run(() => {
     function nop() {};
     var i = 1000000;
     while (i--) {
@@ -95,10 +103,12 @@ on(Locales[0])
     a += 1
     console.log(a)
   })
-.catch((err) => console.log(err))
+  .catch((err) => console.log(err))
 
 on(Locales[0])
-  .use({b: 8})
-  .then(() => b * 2)
+  .with({
+    b: 8
+  })
+  .run(() => b * 2)
   .then((result) => console.log("result", result))
   .catch((err) => console.log(err))
