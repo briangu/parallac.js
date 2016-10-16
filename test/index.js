06daade36aca3cde0ecc713ab8a92e87a9d00c8e
Brian Guarraci <brian@crickethealth.com>
2016-10-15 20:43:51 -0700

    add tests

---
+++ test/index.js
@@ -0,0 +1,43 @@
+var chai = require("chai");
+var chaiAsPromised = require("chai-as-promised");
+chai.should();
+chai.use(chaiAsPromised);
+
+// var expect = chai.expect;
+// var assert = chai.assert;
+
+var run = require('../parallac').run
+
+describe("parallac tests", function () {
+  describe("test locale ids", function () {
+    it("here.id on locale 0", function () {
+      return run(() => {
+        var assert = require('assert')
+        assert.equal(0, here.id, "locale id should be 0")
+        return here.id
+      })
+      .should.eventually.be.equal(0)
+    })
+    it("here.id on each locale", function () {
+      return run(() => {
+        var calls = []
+        for (let i = 0; i < Locales.length; i++) {
+          let locale = Locales[i]
+          calls.push(on(locale)
+            .with({
+              i: i + 1
+            })
+            .do(() => {
+              var assert = require('assert')
+              assert.equal(5, here.id, "locale id should be " + i)
+            }))
+        }
+        return Promise.all(calls)
+          .catch((err) => {
+            throw "test failed: " + "here.id on each locale"
+          })
+      })
+      .should.be.rejected
+    })
+  })
+})
