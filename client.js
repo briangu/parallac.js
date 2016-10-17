0cc14e1999a3543f094499c5781c52adc3ac799f
Brian Guarraci <brian@crickethealth.com>
2016-10-17 09:01:05 -0700

    request response working

--- client.js
+++ client.js
@@ -1,11 +1,43 @@
+var uuid = require('uuid')
+
 var writeln = console.log
+
+var fn = () => {
+  writeln()
+  writeln("test: hello from each locale")
+  for (let locale of Locales) {
+    on(locale).do(() => writeln("hello from locale", here.id))
+  }
+}
+
+var fnReturn = () => {
+  var results = []
+  for (let locale of Locales) {
+    on(locale)
+      .do(() => here.id)
+      .then((result) => results.push(result))
+  }
+  return results
+}
+
+function createFnRequest(id, fn) {
+  const payload = {
+    id: id,
+    fn: JSON.stringify(fn.toString())
+  }
+  return JSON.stringify(payload)
+}
+
+var sessions = {}
 
 var socket = require('socket.io-client')('http://localhost:3000');
 socket.on('connect', function () {
   writeln("connect")
+  socket.emit('event', createFnRequest(uuid.v4(), fn))
+  socket.emit('event', createFnRequest(uuid.v4(), fnReturn))
 });
 socket.on('event', function (data) {
-  writeln("event")
+  writeln("event", data)
 });
 socket.on('disconnect', function () {
   writeln("disconnect")
--- server.js
+++ server.js
@@ -5,22 +5,8 @@ var parallac = require('./parallac')
 var on = parallac.on
 var writeln = console.log
 
-var fn = () => {
-  writeln()
-  writeln("test: hello from each locale")
-  for (let locale of Locales) {
-    on(locale).do(() => writeln("hello from locale", here.id))
-  }
-}
-
-const onRequestPayload = {
-  fn: JSON.stringify(fn.toString())
-}
-const onRequest = JSON.stringify(onRequestPayload)
-
 // TODO: sessions
 // TODO: remote locales
-// TODO: deserialize on request from client
 
 function startServer(config) {
   console.log("hello")
@@ -38,10 +24,19 @@ function startServer(config) {
     socket.on('disconnect', function(){
       console.log('user disconnected');
     });
-    // TODO: deserialize on request from client
-    var req = JSON.parse(onRequest)
-    reqFn = JSON.parse(req.fn)
-    on(config.Locales[0]).do(reqFn)
+    socket.on('event', function (data) {
+      console.log(data)
+      var req = JSON.parse(data)
+      reqFn = JSON.parse(req.fn)
+      on(config.Locales[0])
+        .do(reqFn)
+        .then((result) => {
+          socket.emit('event', {
+            id: req.id,
+            result: result
+          })
+        })
+    })
   });
 }
 
