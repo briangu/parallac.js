bc4f72d5a9b24e4c3e932ba50cc2436c33cb8617
Brian Guarraci <brian@crickethealth.com>
2016-10-22 01:31:19 -0700

    add sessions

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
-var app = require('express')();
+var app = require('express')()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
 var parallac = require('../lib/parallac')
 var on = parallac.on
 var writeln = console.log
+var uuid = require('uuid')
 
 // TODO: sessions
 // TODO: remote locales
@@ -11,11 +12,9 @@ var writeln = console.log
 function startServer(config) {
   console.log("hello")
 
-  // PROTO: rewrite writeln to remote over ws
-
   app.get('/', function (req, res) {
-    res.sendfile('index.html');
-  });
+    res.sendfile('index.html')
+  })
 
   app.post('/event', function (req, res) {
     console.log("post handler", data)
@@ -29,22 +28,17 @@ function startServer(config) {
           result: result
         })
       })
-  });
+  })
 
   http.listen(3000, function () {
-    console.log('listening on *:3000');
-  });
+    console.log('listening on *:3000')
+  })
 
   io.on('connection', function (socket) {
-    console.log('a user connected');
-    socket.on('disconnect', function () {
-      console.log('user disconnected');
-    });
-    socket.on('run', function (data) {
-      var req = JSON.parse(data)
-      reqFn = JSON.parse(req.fn)
-      // TODO: req.id should be generated here and sent back to client
-      var sessionLocales = config.Locales.map((locale) => locale.createSessionContext(req.id))
+    console.log('a user connected')
+
+    function createSession(sessionId) {
+      var sessionLocales = config.Locales.map((locale) => locale.createSessionContext(sessionId))
       for (let locale of sessionLocales) {
         locale.context().Locales = sessionLocales
         locale.context().writeln = function () {
@@ -54,27 +48,71 @@ function startServer(config) {
             values.push(arguments[k])
           }
           socket.emit('writeln', {
-            id: req.id,
+            id: sessionId,
             args: JSON.stringify(values)
           })
         }
       }
-      on(sessionLocales[0], req.id)
-        .do(reqFn)
-        .then((result) => {
-          socket.emit('result', {
-            id: req.id,
-            result: result
-          })
-        })
-        .then(() => {
-          config.Locales.forEach((locale) => locale.destroySessionContext(req.id))
-        })
-        .catch(() => {
-          config.Locales.forEach((locale) => locale.destroySessionContext(req.id))
-        })
+      return sessionLocales
+    }
+
+    function destroySession(sessionId) {
+      console.log("destroying session", sessionId)
+      config.Locales.forEach((locale) => locale.destroySessionContext(sessionId))
+    }
+
+    var sessions = {}
+
+    socket.on('connect', function () {
+      console.log('user connected')
     })
-  });
+
+    socket.on('disconnect', function () {
+      console.log('user disconnected')
+      let remainingSessions = Object.keys(sessions)
+      if (remainingSessions.length > 0) {
+        console.log("abandoning sessions: ", remainingSessions)
+        remainingSessions.forEach((sessionId) => {
+          destroySession(sessionId)
+          delete sessions[sessionId]
+        })
+      }
+    })
+
+    socket.on('create_session', function (req) {
+      console.log("creating session", req.id)
+
+      if (req.id in sessions) {
+        console.log("WARNING: session id already exists!", req.id)
+      }
+
+      sessions[req.id] = createSession(req.id)
+    })
+
+    socket.on('destroy_session', function (req) {
+      destroySession(req.id)
+      delete sessions[req.id]
+    })
+
+    socket.on('on', function (req) {
+      reqFn = JSON.parse(req.fn)
+      let sessionLocales = sessions[req.id]
+      if (sessionLocales) {
+        let here = sessionLocales[0] // TODO: find here
+
+        on(here, req.id)
+          .do(reqFn)
+          .then((result) => {
+            socket.emit('result', {
+              id: req.id,
+              result: result
+            })
+          })
+      } else {
+        console.log("on", "unknown session", req.id)
+      }
+    })
+  })
 }
 
 parallac
