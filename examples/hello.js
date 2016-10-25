79c393af829abf94a087e8aef4f5914d0f00b8f3
Brian Guarraci <brian@crickethealth.com>
2016-10-25 09:09:56 -0700

    blocking example works

--- examples/hello.js
+++ examples/hello.js
@@ -5,7 +5,9 @@ var run = parallac.run
 
 run(() => {
   writeln("hello from each locale:")
-  for (let locale of Locales) {
-    on(locale).do(() => writeln("hello from locale", here.id))
-  }
+  // TODO: catch array of Promises in framework and wrap with Promise.all
+  var calls = Locales.map((locale) =>
+    on(locale).do(() =>
+      writeln("hello from locale", here.id)))
+  return Promise.all(calls)
 })
