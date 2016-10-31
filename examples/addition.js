23a0071e1c85acf4b870578f7adc6d6b9e3ca8fd
Brian Guarraci <brian@crickethealth.com>
2016-10-31 08:29:29 -0700

    wip

--- examples/addition.js
+++ examples/addition.js
@@ -7,6 +7,7 @@ run(() => {
   return createDomain(Locales, 16)
     .then((d) => createDistArray(d))
     .then((a) => a.setAll(1))
+    .then((a) => a.map().do((x) => writeln(x)))
     .then((a) => a.forAll().set((i) => i))
     .then((a) => a.map().do((x) => writeln(x)))
     .then((a) => a.getAll())
