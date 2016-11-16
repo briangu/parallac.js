c3fa76763c2c0dc4e980c4765682182ca7bc79de
Brian Guarraci <brian@crickethealth.com>
2016-11-15 18:12:10 -0800

    use int instead of uuid for request id

--- bin/stress.sh
+++ bin/stress.sh
@@ -1,7 +1,7 @@
  #!/bin/bash
 for i in `seq 1 1000`;
 do
-        node ../examples/addition
-        node ../examples/addition2
-        node ../examples/addition3
+        node examples/addition
+        node examples/addition2
+        node examples/addition3
 done
